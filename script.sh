53e830ca5be4a18953b4b54a4932aa8ad5335eb4
Maria KORNIIETS <mkorniie@e1r1p15.unit.ua>
2018-09-08 17:24:46 +0300

    Useless file removed

--- script.sh
+++
@@ -1,9 +0,0 @@
-MY_ASM="$HOME/Documents/CoreWar/asm"
-ASM="$HOME/Downloads/vm_champs/asm"
-
-# while [[  ]] ; do
-
-${MY_ASM} $1.s; ${ASM} $1.s
-xxd $1.cor > a1 && xxd $1.cor > a2 && diff a1 a2
-
-# end;
